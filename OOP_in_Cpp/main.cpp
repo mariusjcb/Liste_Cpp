d7abdcdd0215663a448f838fac66981aa9a06593
Marius Ilie <marius_ilie20082001@yahoo.com>
2017-05-13 16:44:11 +0300

    Initial Commit

---
+++ OOP_in_Cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOP_in_Cpp
+//
+//  Created by Marius Ilie on 13/05/17.
+//  Copyright © 2017 Marius Ilie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
